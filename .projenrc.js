c7a9757777f03c1bed65834f083120fb457bd731
Nicolas Long <nicolas.long@theguardian.com>
2022-06-16 10:15:28 +0100

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Nicolas Long',
+  authorAddress: 'nicolas.long@theguardian.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-v2',
+  repositoryUrl: 'https://github.com/nicolas.long/cdk-v2.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
